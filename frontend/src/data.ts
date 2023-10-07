fb1602bc96993d89c7afcc76f54b70aaf94bec05
Hemaka <hemaka.tropusi@gmail.com>
2023-10-07 16:34:32 +0530

    food model, service added,

---
+++ frontend/src/data.ts
@@ -0,0 +1,14 @@
+import { Food } from "./app/shared/models/Food";
+
+export const sample_foods: Food[] = [
+    {
+        id: '1',
+        name: 'vegitable pizza',
+        price: 9,
+        favorite: true,
+        stars: 3,
+        imageUrl: 'assets/veg-pizza.jpg',
+        origins: ['India', 'asia'],
+        cookTime: '40-30'
+    }
+]
